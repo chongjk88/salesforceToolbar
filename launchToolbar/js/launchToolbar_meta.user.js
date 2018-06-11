66367755f7828dc47e00834e205516d98a606d9c
Jun Kim <chongjk88@hotmail.com>
2018-06-11 09:36:29 -0700

    Change

--- launchToolbar/js/launchToolbar_meta.user.js
+++ launchToolbar/js/launchToolbar_meta.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name Salesforce Launch Toolbar
+// @name Salesforce Launch Toolbar v4.8.3
 // @namespace www.cobaltgroup.com
 // @include https://cdk.my.salesforce.com/a1E*
 // @include https://cdk--c.na57.visual.force.com/*
@@ -10,7 +10,7 @@
 // @resource fontAwe https://use.fontawesome.com/3953f47d82.js
 // @resource googFont https://fonts.googleapis.com/css?family=Raleway:600,900
 // @resource jqueryUI https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css
-// @require https://raw.githubusercontent.com/chongjk88/salesforceToolbar/master/launchToolbar/js/launchToolbar.js
+// @require https://raw.githubusercontent.com/chongjk88/salesforceToolbar/v4.8.3-pre-release/launchToolbar/js/launchToolbar.js
 // @author cire
 // @grant unsafeWindow
 // @grant GM_setClipboard
